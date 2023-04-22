baf05bbf5f854f210d5db0274abc198fca6dc8e0
Jean Lucas Lima <jeanleonino@gmail.com>
2023-04-22 16:29:01 -0300

    Duplicates implemented
    - Count duplicates
    - Btn kills duplicates

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,23 @@
-
+// all current arrays
 const tabs = await chrome.tabs.query({
   url: [
     '<all_urls>'
   ]
 });
 
-const audible = tabs.filter(tab => { return tab.audible; });
+// new array with only tabs where audible is true
+const audible = tabs.filter(tab => tab.audible);
+
+// new array with all the duplicated tabs
+const duplicates = tabs.filter((item, index, arr) => {
+  return arr.slice(index + 1).some(otherItem => otherItem.url === item.url);
+});
 
 document.querySelector('.number-tabs-open').append(tabs.length);
 document.querySelector('.number-audible').append(audible.length);
+document.querySelector('.number-repeated').append(duplicates.length);
 
-
+// To sort tabs by Title 
 //const collator = new Intl.Collator();
 //tabs.sort((a, b) => collator.compare(a.title, b.title));
 
@@ -18,10 +25,10 @@ const template = document.getElementById('li_template');
 const elements = new Set();
 for (const tab of tabs) {
   const element = template.content.firstElementChild.cloneNode(true);
-
+  
   const title = tab.title.split('-')[0].trim();
   const pathname = new URL(tab.url);
-
+  
   element.querySelector('.title').textContent = title;
   element.querySelector('.pathname').textContent = pathname;
   element.querySelector('a').addEventListener('click', async () => {
@@ -29,8 +36,8 @@ for (const tab of tabs) {
     await chrome.tabs.update(tab.id, { active: true });
     await chrome.windows.update(tab.windowId, { focused: true });
   });
-
-
+  
+  
   element.querySelector('.btn-close-tab').addEventListener('click', async () => {
     await chrome.tabs.remove(tab.id);
     //element.querySelector('.btn-close-tab').innerHTML = "tab removed";
@@ -64,32 +71,26 @@ const search_query = document.querySelector('input[type="search"]');
 search_query.addEventListener('search', async () => {
   let tl = document.createElement('li');
   tl.innerHTML = 'Results for: ' + search_query.value;
-
+  
   document.querySelector('ul').innerHTML = '';
   document.querySelector('ul').appendChild(tl);
-
-  const tabs_search = await chrome.tabs.query({
-    url: [
-      "<all_urls>"
-    ]
-  });
-
-  var results = [];
-
-  Object.values(tabs_search).forEach((val) => {
-      // searches for url
-      if(val.url.includes(search_query.value.toLowerCase())) { 
-          if(!results.includes(val.id)) { results.push(val) }
-      }
   
-      // searches for title
-      if(val.title.includes(search_query.value.toLowerCase())) {         
-          if(!results.includes(val.id)) { results.push(val) }
-      }
+  var results = [];
+  
+  Object.values(tabs).forEach((val) => {
+    // searches for url
+    if(val.url.includes(search_query.value.toLowerCase())) { 
+      if(!results.includes(val.id)) { results.push(val) }
+    }
+    
+    // searches for title
+    if(val.title.includes(search_query.value.toLowerCase())) {         
+      if(!results.includes(val.id)) { results.push(val) }
+    }
   });  
-
+  
   const elements = new Set();
-
+  
   for (const tab of results) {
     const element = template.content.firstElementChild.cloneNode(true);
     
@@ -103,7 +104,7 @@ search_query.addEventListener('search', async () => {
       await chrome.tabs.update(tab.id, { active: true });
       await chrome.windows.update(tab.windowId, { focused: true });
     });
-
+    
     element.querySelector('.btn-close-tab').addEventListener('click', async () => {
       await chrome.tabs.remove(tab.id);
       //element.querySelector('.btn-close-tab').innerHTML = "tab removed";
@@ -113,10 +114,10 @@ search_query.addEventListener('search', async () => {
     
     elements.add(element);
   }
-
+  
   document.querySelector('ul').append(...elements);
   document.querySelector('.tab_counter').innerHTML = "Found " + elements.size + " tabs matching.";
-
+  
   const btn_group_results = document.querySelector('#group_results');
   
   btn_group_results.addEventListener('click', async () => {
